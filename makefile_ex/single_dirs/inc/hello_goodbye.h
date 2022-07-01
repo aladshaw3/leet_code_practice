6e36df4c0ee6277dd35af40c3df0ad6f08737b49
Austin Ladshaw <ladshawap@ornl.gov>
2022-07-01 14:13:43 -0400

    Created nicer looking project with more abstracted makefile

---
+++ makefile_ex/single_dirs/inc/hello_goodbye.h
@@ -0,0 +1,11 @@
+// Prevent duplication of recipies
+#pragma once
+
+// Angle brackets are from installed libraries
+#include <iostream>
+
+// Also include hello.h because we are going to use its function
+#include "hello.h"
+
+// Define the name of a function, its args, and what it returns
+void hello_goodbye();
---
+++ makefile_ex/single_dirs/src/hello_goodbye.cpp
@@ -0,0 +1,12 @@
+// Add include for the "hello_goodbye.h"
+#include "hello_goodbye.h"
+
+// Define how the function 'hello' behaves
+void hello_goodbye()
+{
+  // Call the function from hello.h, which was included in hello_goodbye.h
+  hello();
+
+  // Add an additional message 
+  std::cout << "Goodbye" << std::endl;
+}
