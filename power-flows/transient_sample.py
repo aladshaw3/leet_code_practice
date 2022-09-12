a269ee480e8a2ef599e81ed8b34adec8c891f2ac
Austin Ladshaw <austin.ladshaw@span.io>
2022-09-12 09:41:10 -0700

    Worked more on stability example

--- power-flows/transient_sample.py
+++ power-flows/transient_sample.py
@@ -6,23 +6,6 @@ from scipy.integrate import solve_ivp
 import numpy as np
 import math
 
-'''
-def lotkavolterra(t, z, a, b, c, d):
-    x, y = z
-    return [a*x - b*x*y, -c*y + d*x*y]
-
-sol = solve_ivp(lotkavolterra, [0, 15], [10, 5], args=(1.5, 1, 3, 1),
-                dense_output=True)
-
-t = np.linspace(0, 15, 300)
-z = sol.sol(t)
-
-plt.plot(t, z.T)
-plt.xlabel('t')
-plt.legend(['x', 'y'], shadow=True)
-plt.title('Lotka-Volterra System')
-plt.show()
-'''
 
 # Sample problem:
 """
@@ -30,6 +13,9 @@ plt.show()
 """
 def current_in_circuit(t, i, V, L, w, a, R):
     return 2**0.5*V/L * np.sin(w*t+a) - (R/L)*i
+
+def explicit_step(i_old, dt, t, V, L, w, a, R):
+    return i_old + dt*current_in_circuit(t, i_old, V, L, w, a, R)
 
 def current_in_circuit_exact(t, V, L, w, a, R):
     Z = (R**2 + (w*L)**2)**0.5
@@ -47,13 +33,30 @@ L = 8./w
 t0= 0
 tf = 30
 
+
+
+t_exp = []
+i_exp = []
+i_exp.append(io)
+t_exp.append(t0)
+t_sim = 0
+dt = 1
+
 dt = 0.5*min(max((1/w)*(math.pi/2 - a),(1/w)*(math.pi/2 + a)), (1/w)*(math.pi/2))
+
+i=0
+while t_sim < tf:
+    t_sim = dt+t_sim
+    i+=1
+
+    t_exp.append(t_sim)
+    i_exp.append(explicit_step(i_exp[i-1], dt, t_exp[i], V, L, w, a, R))
+
 
 sol = solve_ivp(current_in_circuit, t_span=[t0, tf], y0=[io],
                 args=(V, L, w, a, R), dense_output=True, max_step=dt,
                 method='RK45', vectorized=True, first_step=dt)
 
-print(sol)
 
 # Plot just the calculated points
 t = sol.t
@@ -65,9 +68,11 @@ t = np.linspace(t0, tf, 1000)
 z_exact = current_in_circuit_exact(t, V, L, w, a, R)
 plt.plot(t, z_exact)
 
+#plt.plot(t_exp, i_exp, marker = '.')
 
 plt.xlabel('t')
-plt.legend(['i_numerical', 'i_exact'], shadow=True)
+plt.legend(['i_numerical', 'i_exact', 'i_bad'], shadow=True)
+#plt.legend(['i_exact', 'i_stable_explicit'], shadow=True)
 plt.title('Current in Circuit')
 plt.savefig('Result.png')
 plt.show()
