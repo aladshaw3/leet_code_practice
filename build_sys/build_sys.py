db0da74d3977d550cf7c673756512b8118722b29
Austin Ladshaw <aladshaw3@outlook.com>
2022-10-19 10:58:07 -0700

    Cleaned up the file

--- build_sys/build_sys.py
+++ build_sys/build_sys.py
@@ -1,4 +1,5 @@
 
+## This is the set of targets that need to be built 
 targets = {'a': {'dependencies': ['b', 'c'], 'build_time': 10},
            'b': {'dependencies': ['d', 'c'], 'build_time': 1},
            'c': {'dependencies': None, 'build_time': 5},
@@ -16,24 +17,48 @@ targets = {'a': {'dependencies': ['b', 'c'], 'build_time': 10},
 # Target 'f' has no dependencies
 # Target 'g' has no dependencies
 
-class BuildTarget:
-    # You can modify stuff in here
 
+## Class object for single build target
+class BuildTarget:
+    ### You can modify stuff in here
+
+    # Constructor for BuildTarget
+    #
+    #   return -> BuildTarget (instance of this class)
+    #
+    #   @param name The name of the target
+    #   @param build_time The amount of time that target needs to build
     def __init__(self, name, build_time):
         # You can modify stuff in here
         self._name = name
         self._build_time = build_time
 
+    # Simple function to build/display the output of building the target
+    #
+    #   return -> None
+    #
     def build(self):
-        # You can modify stuff in here
+        ### You can modify stuff in here
 
-        # Do not modify these lines
+        #### Do not modify these lines ###
         print("Built Target: '" + str(self._name) + "' Took: " + str(self._build_time) + "s")
-    # Feel free to add methods to this class
 
+    # Feel free to add methods to this class if needed
+
+
+## Class object for the logic of building all targets
 class BuildScript:
     ### You can modify stuff in here
 
+    # Constructor for the BuildScript
+    #
+    #   NOTE: You can use this to add in any additional
+    #   build logic you want at this stage, or add in
+    #   any additional objects you want.
+    #
+    #   return -> BuildScript (instance of this class)
+    #
+    #   @param targets A dict object like the example at the top
     def __init__(self, targets):
 
         ### You can modify stuff in here
@@ -71,6 +96,22 @@ class BuildScript:
             # End while loop
 
 
+    # Function to perform the simulated build
+    #
+    #   Your function should call the 'build' function from the
+    #   'BuildTarget' helper class to specify what target is being
+    #   currently built and how long it takes to build. You should
+    #   also be able to supress this output.
+    #
+    #   return -> None
+    #
+    #   @param quite When set to 'True', should suppress output from 'build'
+    #   @param use_parallel (Optional) When set to 'True', should build as though done in parallel
+    #
+    #   NOTE: You are not expected to actually implement any parallelism here, just
+    #   a calculation of what the expected build time would be if using maximum/perfect
+    #   parallelism when building the targets that can be built all at same time. This
+    #   is a theoretical excerise here.
     def build(self, quite=False, use_parallel = False):
         ### You can modify stuff in here
 
@@ -91,6 +132,11 @@ class BuildScript:
             if use_parallel:
                 self._total_time = self._total_time + max_time
 
+    # Function to calculate sequential build time
+    #
+    #   return -> time (int)
+    #
+    #   @param quite When set to 'True', should suppress output from 'build'
     def calculate_build_time(self, quite=True):
         ### You can modify stuff in here
 
@@ -100,6 +146,10 @@ class BuildScript:
 
     # If you could build multiple targets at the same time,
     # what would the build time be?
+    #
+    #   return -> time (int)
+    #
+    #   @param quite When set to 'True', should suppress output from 'build'
     def calculate_build_time_parallel(self, quite=True):
         ### You can modify stuff in here
 
@@ -114,11 +164,11 @@ if __name__=="__main__":
     ## Test 1: Make sure you have 'roughly' the correct build order
     test.build()
 
-    # should print something to the order of
+    # should print something to the order of (exact order can vary)
+    # Built Target: 'c' Took: 5s
     # Built Target: 'e' Took: 9s
     # Built Target: 'f' Took: 8s
     # Built Target: 'g' Took: 7s
-    # Built Target: 'c' Took: 5s
     # Built Target: 'd' Took: 2s
     # Built Target: 'b' Took: 1s
     # Built Target: 'a' Took: 10s
@@ -135,7 +185,7 @@ if __name__=="__main__":
         print("Expected Build Time: " + str(42))
 
 
-    ## Test 3: Calculate parallel build time 
+    ## Test 3: Calculate parallel build time
     time = test.calculate_build_time_parallel()
     if time == 22:
         print("Test 3: PASSED")
