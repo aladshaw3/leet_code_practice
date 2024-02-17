9b025f6c1872de03fbbf13fee98f6f1043d7deca
Austin Ladshaw <aladshaw3@outlook.com>
2024-02-16 21:16:22 -0800

    Saving work

--- makefile_ex/basic/hello.h
+++ makefile_ex/basic/hello.h
@@ -3,6 +3,39 @@
 
 // Angle brackets are from installed libraries
 #include <iostream>
+#include <chrono>
+#include <memory>
+#include <cstddef>
 
 // Define the name of a function, its args, and what it returns
 void hello();
+
+template <typename input_t, typename output_t, typename TimeBase, size_t SAMPLE_TIME>
+class iAlgo {
+	public:
+		TimeBase sample_time;
+		
+		iAlgo() : sample_time(SAMPLE_TIME) {};
+		~iAlgo() { }
+		
+		virtual void Compute(std::shared_ptr<const input_t> input, std::shared_ptr<output_t> output) = 0;
+		
+		virtual void Compute(const input_t& input, output_t& output) = 0;
+};
+
+template <typename TimeBase, size_t SAMPLE_TIME>
+class Algo : public iAlgo<float, float, TimeBase, SAMPLE_TIME> {
+	public:
+		Algo() : iAlgo<float, float, TimeBase, SAMPLE_TIME>() {};
+		
+		void Compute(std::shared_ptr<const float> input, std::shared_ptr<float> output) override { 
+			*output += *input + 1; 
+		}
+		
+		void Compute(const float& input, float& output) override { 
+			output += input + 1; 
+		}
+};
+
+
+
--- makefile_ex/basic/main.cpp
+++ makefile_ex/basic/main.cpp
@@ -5,5 +5,25 @@
 int main()
 {
     hello();
+	Algo<std::chrono::milliseconds, 500> test;
+	auto in = std::make_shared<float>(1.0);
+	std::cout << in.use_count() << std::endl;
+	auto out = std::make_shared<float>(0.0);
+	
+	std::cout << *in << std::endl;
+	*in = 5;
+	std::cout << *in << std::endl;
+	
+	std::cout << *out << std::endl;
+	test.Compute(*in, *out);
+	std::cout << *out << std::endl;
+	
+	//std::cout << in.use_count() << std::endl;
+	
+	test.Compute(in, out);
+	std::cout << *out << std::endl;
+	
+	test.Compute(in, out);
+	std::cout << *out << std::endl;
     return 0;
 }
